cbf132939e1b2c5d5d15f57995b578b32272e7f5
Makson-18 <156922153+Makson-18@users.noreply.github.com>
2026-07-02 20:45:20 +0500

    Update load.py

--- load.py
+++ load.py
@@ -1,3 +1,4 @@
+# импорт библиотек
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import numpy as np
@@ -8,43 +9,39 @@ import tensorflow as tf
 # Загрузка модели
 model = tf.keras.models.load_model("tomato.keras")
 
-# Список классов (10 болезней + здоровый)
-CLASS_NAMES = [
-    "Tomato___Bacterial_spot",
-    "Tomato___Early_blight",
-    "Tomato___healthy",
-    "Tomato___Late_blight",
-    "Tomato___Leaf_Mold",
-    "Tomato___Septoria_leaf_spot",
-    "Tomato___Spider_mites_Two-spotted_spider_mite",
-    "Tomato___Target_spot",
-    "Tomato___Tomato_mosaic_virus",
-    "Tomato___Tomato_yellow_Leaf_Curl_Virus",
-]
-# FastAPI
+# Список классов (9 болезней + здоровый)
+CLASS_NAMES = ['Tomato___Bacterial_spot', 
+    'Tomato___Early_blight', 
+    'Tomato___Late_blight', 
+    'Tomato___Leaf_Mold', 
+    'Tomato___Septoria_leaf_spot', 
+    'Tomato___Spider_mites Two-spotted_spider_mite',
+    'Tomato___Target_Spot', 
+    'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 
+    'Tomato___Tomato_mosaic_virus', 
+    'Tomato___healthy']
+
 app = FastAPI()
 
-@app.get("/")
+
+@app.get("/")# главный файл
 def root():
-    # Возвращаем HTML‑страницу 
     return FileResponse("alert.html")
 
-@app.post("/predict")
+
+@app.post("/predict")# серверная часть
 async def predict(file: UploadFile = File(...)):
-    # Читаем файл
-    contents = await file.read()
-    # Открываем, ресайзим
-    img = Image.open(io.BytesIO(contents)).resize((150, 150))
-    # Нормализуем и добавляем размерность батча
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    
-    # Предсказание
-    pred_probs = model.predict(img_array)[0]   # массив вероятностей (10)
-    predicted_idx = np.argmax(pred_probs)       # индекс класса
-    confidence = float(pred_probs[predicted_idx])
-    
+    read = await file.read()# получаем файл в виде бинарных чисел
+    img = Image.open(io.BytesIO(read)).resize((150, 150))# загружаем в оперативную память, чтобы получиться array(массив)
+    img_array = np.array(img) / 255.0# нормализуем
+    img_array = np.expand_dims(img_array, axis=0)# размерность
+
+    prediction = model.predict(img_array)[0]# получаем 1 вероятность
+    prediction_index = np.argmax(prediction)# класс
+
+    confidence = float(prediction[prediction_index])# строка с уверенностью
+
     return {
-        "класс": CLASS_NAMES[predicted_idx],
-        "уверенность": f"{confidence:.4f}"
+        "класс": CLASS_NAMES[prediction_index], # название болезни из списка
+        "уверенность": f"{confidence * 100:.2f}%" # переводим вероятность в проценты
     }
